a1d52a740c6b08cc541d09cd99710d59ad66681c
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-05-31 13:16:49 +0300

    Update db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_31_002818) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_31_094630) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_31_002818) do
     t.float "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "group_id", null: false
+    t.index ["group_id"], name: "index_operations_on_group_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -60,4 +62,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_31_002818) do
   add_foreign_key "group_operations", "groups"
   add_foreign_key "group_operations", "operations"
   add_foreign_key "groups", "users"
+  add_foreign_key "operations", "groups"
 end
