6790f80348cab9479025d13efd6d16402c5173f0
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-05-31 10:41:21 +0300

    Add operation, user and group module tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  default_url_options :host => "example.com"
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,7 +8,21 @@ RSpec.describe User, type: :model do
       user.password = '123df2'
     end
   end
+
+  it 'should be valid' do
+    expect(subject).to be_valid
+  end
+
+  it 'name should be exist' do
+    expect(subject.name).to eql('Abdimulhin')
+  end
+
   it 'is valid with a name, email, and password' do
     expect(subject).to be_valid
+  end
+
+  it 'fails because password should have atleast 6 characters' do
+    subject.password = '123'
+    expect(subject).to_not be_valid
   end
 end
