ad215aabc00b81e4a2ffab02c3476f89d20b312e
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-05-31 12:03:39 +0300

    Add group controller methods

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,3 +1,25 @@
 class GroupsController < ApplicationController
-  def index; end
+  def index
+    @categories = Group.all.where(user: current_user).includes(:operations)
+  end
+
+  def new
+    @category = Group.new
+  end
+
+  def create
+    @category = Group.new(category_params)
+    @category.user = current_user
+    if @category.save
+      redirect_to group_url
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:group).permit(:name, :icon)
+  end
 end
